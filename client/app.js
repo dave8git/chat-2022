23dddb52a6a314fcdb15503a306defdd7bc9278c
unknown <dave8git@gmail.com>
2022-07-06 01:47:56 +0200

    initial commit

---
+++ client/app.js
@@ -0,0 +1,22 @@
+const loginForm = document.getElementById("add-messages-form");
+const messagesSection = document.getElementById("messages-section__list");
+const messagesList = document.getElementById("messages-list");
+const addMessageForm = document.getElementById("add-messages-form"); 
+const userNameInput = document.getElementById("username");
+const messageContentInput = document.getElementById("message-content");
+
+const userName = ''; 
+
+loginForm.addEventListener('submit', login);
+
+const login = function (e) {
+    alert('working!');
+    e.preventDefault();
+    if(userNameInput.value === '') {
+        alert('Please enter some interesting username!')
+    } else {
+        userName = userNameInput.value;
+        loginForm.classList.remove('show');
+        messagesSection.classList.add('show');
+    }
+}
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const messages = [];
+app.use(express.static(path.join(__dirname, '/client')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/index.html'));
+});
+
+const port = 8000;
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
+
