10f3bc979de7421f20cb12567cf2085d8ccd824a
unknown <dave8git@gmail.com>
2022-07-12 20:24:02 +0200

    add initial server settings

--- client/app.js
+++ client/app.js
@@ -1,17 +1,16 @@
-const loginForm = document.getElementById("add-messages-form");
-const messagesSection = document.getElementById("messages-section__list");
+const loginForm = document.getElementById("welcome-form");
+const messagesSection = document.getElementById("messages-section");
 const messagesList = document.getElementById("messages-list");
 const addMessageForm = document.getElementById("add-messages-form"); 
 const userNameInput = document.getElementById("username");
 const messageContentInput = document.getElementById("message-content");
 
-const userName = ''; 
+let userName = ''; 
 
-loginForm.addEventListener('submit', login);
 
-const login = function (e) {
+const login = function (event) {
     alert('working!'); // should alert, but eventListener not working
-    e.preventDefault();
+    event.preventDefault();
     if(userNameInput.value === '') {
         alert('Please enter some interesting username!')
     } else {
@@ -20,3 +19,29 @@ const login = function (e) {
         messagesSection.classList.add('show');
     }
 }
+
+const sendMessage = function (event) {
+    event.preventDefault();
+    if(messageContentInput.value === '') {
+        alert('Please enter some input!');
+    } else {
+        addMessage(userName, messageContentInput.value);
+        messageContentInput.value = '';
+    }
+}
+
+function addMessage(author, content) {
+    const message = document.createElement('li');
+    message.classList.add('message');
+    message.classList.add('message--received');
+    if(author === userName) message.classList.add('message--self');
+    message.innerHTML = `
+      <h3 class="message__author">${userName === author ? 'You' : author }</h3>
+      <div class="message__content">
+        ${content}
+      </div>
+    `;
+    messagesList.appendChild(message);
+  }
+loginForm.addEventListener('submit', login);
+addMessageForm.addEventListener('submit', sendMessage);
